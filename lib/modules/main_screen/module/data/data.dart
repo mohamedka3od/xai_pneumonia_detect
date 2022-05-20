1779fa8935b7d54c7d4e0c095234f6559ab29c61
Daikan <gukom14@gmail.com>
2022-05-20 17:45:31 +0200

    update2

--- lib/modules/main_screen/module/data/data.dart
+++ lib/modules/main_screen/module/data/data.dart
@@ -12,7 +12,7 @@ class Pro_Data {
     this.id = "Monkey D Luffy" ,
     this.mobilenum = 01123545132,
     this.aga = 69,
-    this.image = 'assets/images/luffy.png',
+    this.image = 'assets/images/DWc1yS.jpg',
     this.email = 'MonkeyDG4@oneP.com'
   });
 
